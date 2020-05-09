b36e111394756365394cf6051552b46664212761
shcomender <54302970+shcomender@users.noreply.github.com>
2020-05-09 23:35:59 +0800

    20200509

--- tetris/Tetris_java/src/main/GameMap.java
+++ tetris/Tetris_java/src/main/GameMap.java
@@ -1,62 +1,107 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Map;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GameMap extends JPanel implements KeyListener{
 	//map数组用来存储整个地图中方块的占位信息
-	public int[][] map = new int[29][14];
+	public int[][] map = new int[27][16];
 	//偏移数组用来计算方块位置相对于中心点的偏移量，方便计算每个小块的位置
 	public int[][] offest = {{-1,-1},{0,-1},{1,-1},
 							 {-1,0},{0,0},{1,0},
 							 {-1,1},{0,1},{1,1} 
 							};
-	public final static int START_HOR = 6,START_VER = 1;
-	public static int hor_index,ver_index;
 	public static int[][] cur_Tetris=null;
+	public static int[][] next_Tetris=null;
+	
+	public final static int START_HOR = 6,START_VER = 1;
+	private static final String TopScore = "0";
+	private static  String Score = "0";
+	public static int hor_index,ver_index;
+	public Timer timer;
+	public boolean Can_Move = true;
+	public static TerisType tp;
 	
 	public GameMap() {
-		this.setBounds(0,0,350,650);
+		this.setBounds(0,0,550,650);
 		this.setBackground(new Color(220, 220, 220));
 		this.setVisible(true);
 		this.setLayout(null);
 		hor_index=START_HOR;
 		ver_index=START_VER;
+		timer = new Timer(1000,this.TimeListener);
+		timer.start();
+		tp = new TerisType();
+		cur_Tetris=tp.getTetris();
+		next_Tetris=tp.getTetris();
+		for(int i=0;i<16;i++)
+			{
+				map[25][i]=1;
+				map[26][i]=1;
+			}
 	}
 	
-	private void move() {
-		// TODO Auto-generated method stub
-			ver_index++;
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println(ver_index);
-			
-	}
+	ActionListener TimeListener = new  ActionListener() {
 
-	//将当前移动的方块的中心坐标和形状输入到地图并显示出来
-	public void changeMap(int[][] src,int hor_index,int ver_index) {
-		cur_Tetris=src;
-		this.hor_index=hor_index;
-		this.ver_index=ver_index;
-		repaint();
-	}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			//判断当方块不能移动了，获取新方块，将当前绘图的样子加入背景数组
+			if(!Can_Move) {
+				putInMap(cur_Tetris);
+				cur_Tetris=next_Tetris;
+				next_Tetris=tp.getTetris();
+				Can_Move=true;
+				hor_index=START_HOR;
+				ver_index=START_VER;
+			}
+			//方块自行下移
+			if(ver_index<23) {
+				ver_index++;
+				if(!isOver(cur_Tetris, 1)) {
+					ver_index--;
+					putInMap(cur_Tetris);
+				}
+			}
+			System.out.println(Can_Move);
+			repaint();
+		}
+		//将当前方块加入背景数组
+		private void putInMap(int[][] cur_Tetris) {
+			// TODO Auto-generated method stub
+			Can_Move=false;
+			int k=0;
+			for(int i=0;i<3;i++) {
+				for(int j=0;j<3;j++) {
+					if(cur_Tetris[i][j]==1)map[ ver_index+offest[k][1] ] [ hor_index+offest[k][0] ]=1;
+					k++;
+				}
+			}
+			
+		}
+		
+	};
 	
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(Color.blue);
-		for(int i=3;i<29;i++) {
+		//绘制已经固定的方块
+		for(int i=0;i<26;i++) {
 			for(int j=0;j<14;j++) {
-				if(map[i][j]!=0)g.fill3DRect((i-3)*25, j*25, 25, 25, false);
+				if(map[i][j]!=0)g.fill3DRect(j*25,i*25, 25, 25, false);
 			}
 		}
+		//绘制当前方块
 		int k=0;
 		for(int i=0;i<3;i++) {
 			for(int j=0;j<3;j++) {
@@ -64,6 +109,25 @@ public class GameMap extends JPanel implements KeyListener{
 				k++;
 			}
 		}
+		//绘制边界
+		g.setColor(Color.black);
+		for(int i=0;i<26;i++) {
+			if(map[i][15]==0)g.fill3DRect(350, i*25, 25, 25, false);
+		}
+		//绘制其他信息
+		g.setFont(new Font("微软雅黑", Font.CENTER_BASELINE, 30));
+		g.drawString("游戏分数:" + this.Score, 380, 50);
+		g.drawString("最高纪录:" + this.TopScore, 380, 100);
+		g.drawString("下一个方块", 380,150);
+		g.setColor(Color.blue);
+		k=0;
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				if(next_Tetris[i][j]!=0)g.fill3DRect(380+j*50,200+i*50, 50,50, false);
+				k++;
+			}
+		}
+		
 	}
 
 	@Override
@@ -114,23 +178,33 @@ public class GameMap extends JPanel implements KeyListener{
 		
 	}
 	
+	//边界检测
 	public boolean isOver(int[][] s,int x) {
 		boolean flag = true;
 		int dep=0,left=0,right=0;
 		for(int i=0;i<3;i++)
 			{
-			if(cur_Tetris[2][i]==1)dep=1;
-			if(cur_Tetris[i][0]==1)left=1;
-			if(cur_Tetris[i][2]==1)right=1;
+			if(s[i][0]==1)left=1;
+			if(s[i][2]==1)right=1;
 			}
-		
 		switch(x) {
-		case 1:if(ver_index+dep>24)flag = false;break;
+		case 1:
+			int k=0;
+			for(int i=0;i<3;i++) {
+				for(int j=0;j<3;j++) {
+					if(s[i][j]==1 && map[ ver_index+offest[k][1]] [ hor_index+offest[k][0] ]==1) {
+						flag=false;
+						Can_Move=false;
+					}
+						k++;
+				}
+			}		
+		break;
 		case 2:if(hor_index+right>13)flag = false;break;
 		case 3:if(hor_index-left<0)flag = false;break;
-		case 4:if(ver_index==24) flag = false;break;
+		case 4:if(hor_index+right >13 || hor_index-left<0 || ver_index==24) flag = false;break;
 		}
-		System.out.println(flag);
+		System.out.println(dep+" "+flag);
 		return flag;
 	}
 	
--- tetris/Tetris_java/src/main/mian.java
+++ tetris/Tetris_java/src/main/mian.java
@@ -17,10 +17,7 @@ import javax.swing.JPanel;
 public class mian{
 
 	//变量声明
-	private final static Tools tool = new Tools();			//工具类
-	private static int cur_sorce = 0;						//当前分数
-	private static int Top_sorce = 0;						//
-	public static JPanel Right_Part;						//右侧面板
+	private final static Tools tool = new Tools();			//工具类		
 	public static GameMap Game_Map;							//地图
 	public static int Hor_index,Ver_index;					//小方块相对于地图左上角位移量
 //	public final static int HOR_START=6,VER_START=1;
@@ -32,63 +29,20 @@ public class mian{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		creatFrame();
-		
 	}
 	
 	public static void creatFrame() {
-		//初始化数据
-		Top_sorce = getTopSorce();
-		cur_sorce =0;
-		
 		//初始化界面
 		JFrame mj = new JFrame("俄罗斯方块");
 		mj.setBounds(300, 50, 550, 650);
 		mj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mj.setLayout(null);
 		mj.setResizable(false);
-		//初始化右侧
+		//初始化地图
 		Game_Map = new GameMap();
 		mj.add(Game_Map);
 		mj.addKeyListener(Game_Map);
-		Right_Part = tool.creatJPanle(350,0,200, 650, mj,new Color(180,180,180));
-		//初始化右侧
-		JLabel SorceTitle = tool.createJLabel(0,0,200,50,40,"分   数",Right_Part);
-		JLabel Sorce = tool.createJLabel(0, 60, 200, 50, 50, Integer.toString(cur_sorce), Right_Part);
-		JLabel TopSorceTitle = tool.createJLabel(0, 120, 200, 50, 40, "最 高 分", Right_Part);
-		JLabel TopSorce = tool.createJLabel(0, 180, 200, 50, 50, Integer.toString(Top_sorce), Right_Part);
-		JLabel NextTetris = tool.createJLabel(0,240,200,50,30,"下一个方块",Right_Part);
-		
-		StartGame();
-		
-		
+
 		mj.setVisible(true);
 	}
-
-	private static void StartGame() {
-		// TODO Auto-generated method stub
-		//初始化提示区域面板
-		TerisType teris_type = new TerisType();
-		//获取初始化方块形状
-		Next_Tetris=teris_type.getTetris();
-		TetirsPaine tp = new TetirsPaine(Next_Tetris);
-		Right_Part.add(tp);
-		//Cur_Tetris=Next_Tetris;
-		Game_Map.changeMap(Next_Tetris, 6, 1);
-	}
-	
-	public void KeyPressed(KeyEvent e) {
-		switch(e.getKeyChar()) {
-		case 'W':System.out.println("1");
-		break;
-		}
-	}
-	
-	
-	//从文件中读取最高分
-	public  static int getTopSorce() {
-		// TODO Auto-generated method stub
-		int temp=0;
-		return temp;
-	}
-
 }
